d958391404ef4daa0fd876e2f37bdc2c8495dbe8
Alex Mendes <alexander.mendes@immediate.co.uk>
2019-02-09 20:48:50 +0000

    feat: add basic demo

---
+++ demo/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <button @click="start">Start</button>
+    <button @click="stop">Stop</button>
+    <select ref="shape">
+      <option value="heart">Heart</option>
+      <option value="rect">Rectangle</option>
+      <option value="circle">Circle</option>
+    </select>
+  </main>
+</template>
+
+<script>
+  export default {
+    methods: {
+      start () {
+        this.$confetti.start({
+          shape: this.$refs.shape.value
+        })
+      },
+
+      stop () {
+        this.$confetti.stop()
+      }
+    }
+  }
+</script>
--- webpack.config.js
+++ webpack.config.js
@@ -1,18 +1,30 @@
 const path = require('path');
 const TerserWebpackPlugin = require('terser-webpack-plugin');
 const CleanWebpackPlugin = require('clean-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const VueLoaderPlugin = require('vue-loader/lib/plugin');
 
 const distDir = path.resolve(__dirname, 'dist');
 
 module.exports = {
-  entry: path.resolve(__dirname, 'src', 'index.js'),
+  entry: {
+    'vue-confetti': [
+      path.resolve(__dirname, 'src', 'index.js')
+    ],
+    demo: [
+      path.resolve(__dirname, 'demo', 'demo.js'),
+    ]
+  },
+
   mode: 'production',
+
   output: {
     path: distDir,
-    filename: 'vue-confetti.js',
+    filename: '[name].js',
     library: 'vue-confetti',
     libraryTarget: 'umd',
   },
+
   module: {
     rules: [
       {
@@ -20,8 +32,13 @@ module.exports = {
         loader: 'babel-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.vue$/,
+        loader: 'vue-loader',
+      },
     ],
   },
+
   optimization: {
     minimizer: [
       new TerserWebpackPlugin({
@@ -30,6 +47,7 @@ module.exports = {
       }),
     ],
   },
+
   plugins: [
     new CleanWebpackPlugin(
       [`./dist`],
@@ -38,5 +56,12 @@ module.exports = {
         verbose: false,
       }
     ),
+
+    new HtmlWebpackPlugin({
+      template: require('html-webpack-template'),
+      appMountId: 'app',
+    }),
+
+    new VueLoaderPlugin(),
   ],
 };
