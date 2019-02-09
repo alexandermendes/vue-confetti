247bb66d3a9d982ae02e4674d4062c0bd98f04ad
Alex Mendes <alexander.mendes@immediate.co.uk>
2019-02-09 20:57:11 +0000

    chore: add styles and npm badge to demo

--- demo/App.vue
+++ demo/App.vue
@@ -1,12 +1,20 @@
 <template>
-  <main>
-    <button @click="start">Start</button>
-    <button @click="stop">Stop</button>
-    <select ref="shape">
+  <main style="display: flex; justify-content: center; align-items: center;">
+    <button @click="start" style="margin: 5px;">Start</button>
+    <button @click="stop" style="margin: 5px;">Stop</button>
+    <select ref="shape" style="margin: 5px;">
       <option value="heart">Heart</option>
       <option value="rect">Rectangle</option>
       <option value="circle">Circle</option>
     </select>
+    <a
+      href="https://badge.fury.io/js/vue-confetti"
+      style="display: flex; margin: 5px;">
+      <img
+        src="https://badge.fury.io/js/vue-confetti.svg"
+        alt="npm version"
+        height="18">
+    </a>
   </main>
 </template>
 
