f46836de76f0e1fdb2c5e24d6289341738bfc9eb
Alex Mendes <alexander.mendes@immediate.co.uk>
2019-02-09 18:06:16 +0000

    chore: upgrade to webpack 4 and babel 7

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,42 @@
-const path = require('path')
-const webpack = require('webpack')
+const path = require('path');
+const TerserWebpackPlugin = require('terser-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+const distDir = path.resolve(__dirname, 'dist');
 
 module.exports = {
   entry: path.resolve(__dirname, 'src', 'index.js'),
+  mode: 'production',
   output: {
-    path: path.resolve(__dirname, 'dist'),
+    path: distDir,
     filename: 'vue-confetti.js',
     library: 'vue-confetti',
-    libraryTarget: 'umd'
+    libraryTarget: 'umd',
   },
   module: {
-    loaders: [
+    rules: [
       {
         test: /\.js$/,
         loader: 'babel-loader',
-        exclude: /node_modules/
-      }
-    ]
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  optimization: {
+    minimizer: [
+      new TerserWebpackPlugin({
+        cache: true,
+        parallel: true,
+      }),
+    ],
   },
   plugins: [
-    new webpack.optimize.UglifyJsPlugin()
-  ]
-}
+    new CleanWebpackPlugin(
+      [`./dist`],
+      {
+        root: path.resolve('..'),
+        verbose: false,
+      }
+    ),
+  ],
+};
