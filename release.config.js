dee97d659b8be3b434848d7e3d8b8d3de9e8ab20
Alex Mendes <alexander.mendes@immediate.co.uk>
2019-02-10 22:00:18 +0000

    fix: add @semantic-release/npm plugin

--- release.config.js
+++ release.config.js
@@ -15,14 +15,7 @@ module.exports = {
         ]
       }
     ],
-      '@semantic-release/release-notes-generator',
-    [
-      '@semantic-release/git', {
-        assets: [
-          'package.json',
-        ],
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-      },
-    ],
+    '@semantic-release/release-notes-generator',
+    '@semantic-release/npm'
   ],
 };
