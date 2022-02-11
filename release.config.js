ef57b6273be15f845685396f57438747e69ced59
Alex Mendes <alexander.mendes@immediate.co.uk>
2022-02-11 23:45:38 +0000

    build: update release config

--- release.config.js
+++ release.config.js
@@ -1,39 +1,10 @@
 module.exports = {
   plugins: [
-    [
-      '@semantic-release/commit-analyzer', {
-        preset: 'angular',
-        releaseRules: [
-          {
-            type: 'refactor',
-            release: 'patch'
-          },
-          {
-            type: 'docs',
-            release: 'patch'
-          },
-          {
-            type: 'build',
-            release: 'patch'
-          },
-          {
-            type: 'ci',
-            release: 'patch'
-          },
-        ]
-      }
-    ],
-    '@semantic-release/release-notes-generator',
+    '@semantic-release/commit-analyzer',
     '@semantic-release/npm',
+    '@semantic-release/changelog',
+    '@semantic-release/release-notes-generator',
     '@semantic-release/github',
-    [
-      '@semantic-release/git', {
-        assets: [
-          'package.json',
-          'package-lock.json'
-        ],
-        message: 'chore(release): ${nextRelease.version} [skip ci]\n\n${nextRelease.notes}',
-      },
-    ],
+    '@semantic-release/git',
   ],
 };
